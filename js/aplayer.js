166e68f0990365ef47a0cf0e5ae1b2d006aecb14
frank.feng <frank.feng@wik-group.com>
2022-12-09 17:33:48 +0800

    Site updated: 2022-12-09 17:33:31

---
+++ js/aplayer.js
@@ -0,0 +1,54 @@
+!(function() {
+  var oldLoadAp = window.onload;
+  window.onload = function () {
+    oldLoadAp && oldLoadAp();
+
+    new APlayer({
+      container: document.getElementById('aplayer'),
+      listFolded: true,
+      listMaxHeight: 90,
+	  lrcType: 3,
+	  fixed: true,
+      autoplay: true,
+	  mutex: true,
+      loop: 'all',
+      order: 'random',
+      theme: '#b7daff',
+      preload: 'auto',
+      audio: [
+        {
+          name: '晴天',
+          artist: '周杰伦',
+          url: '/songs/晴天.mp3',
+          cover: '/img/avatar.png',
+		  lrc: '/songs/晴天.lrc',
+		  theme: '#ebd0c2'
+        },
+		{
+		  name: '告白气球',
+		  artist: '周杰伦',
+		  url: '/songs/告白气球.mp3',
+		  cover: '/img/avatar.png',
+		  lrc: '/songs/告白气球.lrc',
+		  theme: '#46718b'
+		},
+		{
+		  name: '稻香',
+		  artist: '周杰伦',
+		  url: '/songs/稻香.mp3',
+		  cover: '/img/avatar.png',
+		  lrc: '/songs/稻香.lrc',
+		  theme: '#46718b'
+		},
+		{
+		  name: '最伟大的作品',
+		  artist: '周杰伦',
+		  url: '/songs/最伟大的作品.mp3',
+		  cover: '/img/avatar.png',
+		  lrc: '/songs/最伟大的作品.lrc',
+		  theme: '#46718b'
+		}
+      ]
+    });
+  }
+})();
